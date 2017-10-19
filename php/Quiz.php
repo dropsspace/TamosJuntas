7021e82d4563650af54f731df9ea06c990c7530f
Gi Bordignon <contateagiulia@gmail.com>
2017-10-19 17:00:18 -0200

    Criação da Classe Quiz
    Quiz controlará a seleção de perguntas, arrays e inserção de dados no banco

---
+++ php/Quiz.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once 'Pergunta.php';
+include_once 'RegistroUso.php';
+
+class Quiz {
+
+public $arrayRespostas;
+
+public function construct(){
+$arrayRespostas = array('1' => array($idUso, $idResp));
+}
+
+}
+?>
--- php/ResultadoTeste.php
+++ php/ResultadoTeste.php
@@ -18,6 +18,8 @@ class ResultadoTeste {
         ConectaBD::fecharBanco();
     }
 
+   
+
 }
 
 ?>
