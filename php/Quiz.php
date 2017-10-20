0956fe8f82713bd3d70484a47e2882a220acdf5c
Gi Bordignon <contateagiulia@gmail.com>
2017-10-19 22:52:29 -0200

    Aprimoramento Classe Quiz

--- php/Quiz.php
+++ php/Quiz.php
@@ -5,11 +5,42 @@ include_once 'RegistroUso.php';
 
 class Quiz {
 
-public $arrayRespostas;
+    public static $arrayRespostas = array();
+    public static $idUso; 
+    
+    public function construct($dtaAcesso, $filhos, $estuda, $trabalha, $tpRelacionamento)
+            {
+    self::$idUso = RegistroUso::registraUso($dtaAcesso, $filhos, $estuda, $trabalha, $tpRelacionamento);
+               }
 
-public function construct(){
-$arrayRespostas = array('1' => array($idUso, $idResp));
+
+    public function salvaResposta($idResp) {
+        array_push(self::$arrayRespostas, array("idUso" => self::$idUso, "idResp" => $idResp));
+    }
+
+    public function sorteiaPergunta()
+            {
+        
+            }
+    
+
 }
 
+//$idUso1 = 11;
+//$idResp1 = 22;
+//
+//Quiz::salvaResposta($idUso1, $idResp1);
+//$var = Quiz::exibeArray();
+//
+//$idUso2 = 14;
+//$idResp2 = 24;
+//
+//Quiz::salvaResposta($idUso2, $idResp2);
+//$var = Quiz::exibeArray();
+//
+//
+//foreach ($var as $item) {
+//    echo "\n", $item['idUso'], "\t\t", $item['idResp'];
+//    echo"<br>";
 }
 ?>
